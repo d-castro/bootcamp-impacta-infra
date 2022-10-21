51d6b343cd2406fe6113eab2a349817605da288d
Diego Crisostomo Lo Castro Da Silva <diego.crisostomo@hotmail.com>
2022-10-20 21:09:51 -0300

    feat(): create pipeline

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "bootcamp"
+    organization = "LabDevopsCloud-dcastro"
 
     workspaces {
       name = "bootcamp-impacta"
